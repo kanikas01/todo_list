27b73786d0891975901898f870449d4d40674b70
kanikas01 <larvakurt@yahoo.com>
2020-02-21 13:25:22 -0800

    Add basic styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { useQuery } from '@apollo/react-hooks';
-import { gql } from 'apollo-boost';
+import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
 // add todos
 // toggle todos
 // delete todos
@@ -21,19 +21,32 @@ function App() {
   if (loading) return <div>Loading todos...</div>;
   if (error) return <div>Error fetching todos!</div>;
   return (
-    <div>
-      <h1>GraphQL Checklist</h1>
+    <div className="vh-100 code flex flex-column items-center bg-purple white pa3 fl-1">
+      <h1 className="f2-l">
+        GraphQL Checklist{" "}
+        <span role="img" aria-label="Checkmark">
+          ✅
+        </span>
+      </h1>
       {/* Todo Form */}
-      <form>
-        <input type="text" placeholder="write your todo" />
-        <button type="submit">Create</button>
+      <form className="mb3">
+        <input
+          className="pa2 f4 b--dashed"
+          type="text"
+          placeholder="write your todo"
+        />
+        <button className="pa2 f4 bg-green" type="submit">
+          Create
+        </button>
       </form>
       {/* Todo List */}
-      <div>
+      <div className="flex items-center justify-center flex-column">
         {data.todos.map(todo => (
           <p key={todo.id}>
-            <span>{todo.text}</span>
-            <button>&times;</button>
+            <span className="pointer list pa1 f3">{todo.text}</span>
+            <button className="bg-transparent nt bn f4">
+              <span className="red">&times;</span>
+            </button>
           </p>
         ))}
       </div>
