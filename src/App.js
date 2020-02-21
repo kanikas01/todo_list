a88679b9e51ba6c7ae8aad938124f4455c481ab9
kanikas01 <larvakurt@yahoo.com>
2020-02-21 15:26:28 -0800

    Add basic toggleTodo functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useQuery } from "@apollo/react-hooks";
+import { useQuery, useMutation } from "@apollo/react-hooks";
 import { gql } from "apollo-boost";
 // add todos
 // toggle todos
@@ -16,10 +16,30 @@ const GET_TODOS = gql`
   }
 `;
 
+const TOGGLE_TODO = gql`
+  mutation toggleTodo($id: uuid!, $done: Boolean!) {
+    update_todos(where: { id: { _eq: $id } }, _set: { done: $done }) {
+      returning {
+        done
+        id
+        text
+      }
+    }
+  }
+`;
+
 function App() {
   const { data, loading, error } = useQuery(GET_TODOS);
+  const [toggleTodo] = useMutation(TOGGLE_TODO);
+
+  async function handleToggleTodo({ id, done }) {
+    const data = await toggleTodo({ variables: { id: id, done: !done } });
+    console.log(data);
+  }
+
   if (loading) return <div>Loading todos...</div>;
   if (error) return <div>Error fetching todos!</div>;
+
   return (
     <div className="vh-100 code flex flex-column items-center bg-purple white pa3 fl-1">
       <h1 className="f2-l">
@@ -42,8 +62,10 @@ function App() {
       {/* Todo List */}
       <div className="flex items-center justify-center flex-column">
         {data.todos.map(todo => (
-          <p key={todo.id}>
-            <span className="pointer list pa1 f3">{todo.text}</span>
+          <p onDoubleClick={() => handleToggleTodo(todo)} key={todo.id}>
+            <span className={`pointer list pa1 f3 ${todo.done && "strike"}`}>
+              {todo.text}
+            </span>
             <button className="bg-transparent nt bn f4">
               <span className="red">&times;</span>
             </button>
