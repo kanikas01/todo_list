875ce78b4f54f636c9a2ca598c60d50d9a0f1a5a
kanikas01 <larvakurt@yahoo.com>
2020-02-21 11:08:31 -0800

    Add basic query and display functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React from 'react';
-
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
 // add todos
 // toggle todos
 // delete todos
 // list todos
 
+const GET_TODOS = gql`
+  query getTodos {
+    todos {
+      done
+      id
+      text
+    }
+  }
+`;
+
 function App() {
-  return <div>app</div>;
+  const { data, loading, error } = useQuery(GET_TODOS);
+  if (loading) return <div>Loading todos...</div>;
+  if (error) return <div>Error fetching todos!</div>;
+  return (
+    <div>
+      {data.todos.map(todo => (
+        <p key={todo.id}>
+          <span>{todo.text}</span>
+          <button>&times;</button>
+        </p>
+      ))}
+    </div>
+  );
 }
 
 export default App;
